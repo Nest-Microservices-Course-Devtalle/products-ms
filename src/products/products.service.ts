07c8bb06ac971716c62f020cc084ba2ad9944798
memo <memo2705@gmail.com>
2024-08-15 12:18:56 -0700

    products

---
+++ src/products/products.service.ts
@@ -0,0 +1,84 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    return await this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalRegs = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalRegs / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        totalRegs,
+        page,
+        lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id: __, ...data } = updateProductDto;
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    /* return await this.product.delete({
+      where: { id },
+    }); */
+
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+
+    return product;
+  }
+}
